260a552d6cbaff3fc784464fc8236aa1054004e4
Barry Yosilevich <63207303+barryyosi@users.noreply.github.com>
2022-03-30 01:09:57 +0300

    Update shifter.vhd
    not finished

--- shifter.vhd
+++ shifter.vhd
@@ -1,1 +1,25 @@
 
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+--------------------------------------
+ENTITY shifter IS
+	GENERIC (n : INTEGER := 4);
+	PORT (x, y : IN std_logic_vector (n-1 DOWNTO 0);
+	       dir : IN std_logic;
+		  cout : OUT std_logic;
+          res : OUT std_logic_vector (n-1 DOWNTO 0) 
+			);
+END shifter;
+--------------------------------------
+ARCHITECTURE shifterArch OF shifter IS
+BEGIN
+	if dir = 0 then             -- shift left
+        for j in 0 to k-1 generate
+            y(0) <= '0';
+            cout <= y(n-1);
+            for i in 1 to n-1 generate
+              y(i) <= y(i-1);
+  else                        -- dir = 1 -> shift right
+        y
+
+END shifterArch;
