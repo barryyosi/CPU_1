831cdf1a9c4b669b7fffe6f6aead0f9464222dee
barryyosilevich <barry.yosilevich@whitesourcesoftware.com>
2022-04-03 14:31:01 +0300

    shifterCheck

--- VHDL/shifter.vhd
+++ VHDL/shifter.vhd
@@ -14,31 +14,28 @@ END shifter;
 --------------------------------------
 ARCHITECTURE shifterArch OF shifter IS
 
-        signal k : integer := 0;
-          
+        signal zeros : std_logic_vector (n-1 DOWNTO 0 ) <= (others => '0');
+        
 
 BEGIN
-        for l in 0 to n-1 generate
-                if x(i) = '1' then
-                        k <= k + l ** 2;
-        end generate;     
+        if dir = '0' then                       -- shift left
+                for i in 0 to n-1 generate
+                        if x(i) = '1' generate
+                                res <= y( n - 2**i - 1 downto 0) & zeros( 0 to 2**i );         
+                        end generate;
+                end generate;
+        
+        else                                    -- dir = '1' -> shift right
+                for i in 0 to n-1 generate
+                        if x(i) = '1' generate
+                                res <= zeros( 0 to 2**i ) & y( n - 1 downto 2**i) ;         
+                        end generate;
+                end generate;
 
-        if dir = '0' then             -- shift left
-                for j in 0 to k-1 generate
-                        cout <= y(n-1); 
-                        for i in n-1 downto 1 generate
-                                res(i) <= y(i-1); 
-                        end generate;
-                        res(0) <= '0';           
-                end generate;
-        else                        -- dir = 1 -> shift right
-                for j in 0 to k-1 generate
-                        
-                        cout <= y(0);
-                        for i in 0 to n-2 generate
-                                res(i) <= y(i+1);  
-                        end generate;
-                        res(n-1) <= '0';
-               
-                end generate;
+        end if;
 END shifterArch;
+
+
+
+
+
