0d41de3db5f6e8cbdaa64e7f315151b957f66993
Barry Yosilevich <63207303+barryyosi@users.noreply.github.com>
2022-04-03 18:33:56 +0300

    updates shifter 18:34

--- DUT/shifter.vhd
+++ DUT/shifter.vhd
@@ -14,17 +14,17 @@ END shifter;
 ARCHITECTURE shifterArch OF shifter IS
 
         signal zeros : std_logic_vector (n-1 DOWNTO 0 ) := (others => '0');
-        constant log_n: integer := integer(ceil(log2(real(n))))
+        constant log_n: integer := integer(ceil(log2(real(n))));
 
 BEGIN
          
-     loop0: for i in 0 to log_n generate
+     loop0: for i in 0 to log_n - 1 generate
                         cout <= y( n - 2**i )  when (x(i)= '1' and ALUFN ="00") else
                                      y( 2**i - 1) when (x(i)= '1' and ALUFN = "01") else
                                      UNAFFECTED; 
                                       
-                        res <= y( n - 2*i - 1 downto 0) & zeros( 0 to 2*i ) when (x(i)= '1' and ALUFN ="00") else
-                                    zeros( 0 to 2*i) & y( n - 1 downto 2*i)  when (x(i)= '1' and ALUFN = "01") else
+                        res <= y( n - 2**i - 1 downto 0) & zeros( 2**i - 1 downto 0  ) when (x(i)= '1' and ALUFN ="00") else
+                                    zeros( 2**i - 1 downto 0 ) & y( n - 1 downto 2**i)  when (x(i)= '1' and ALUFN = "01") else
                                     UNAFFECTED;   
                 
                 end generate;
