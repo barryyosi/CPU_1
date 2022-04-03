fe8880d73d10e9404cd518f2ed6da7b5835b9fa8
barryyosilevich <barry.yosilevich@whitesourcesoftware.com>
2022-04-03 17:21:31 +0300

    2compile

--- DUT/shifter.vhd
+++ DUT/shifter.vhd
@@ -1,4 +1,3 @@
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 use IEEE.math_real.all;
@@ -6,37 +5,28 @@ use IEEE.math_real.all;
 ENTITY shifter IS
 	GENERIC (n : INTEGER := 4);
 	PORT (x, y : IN std_logic_vector (n-1 DOWNTO 0);
-	        ALUFN : IN std_logic_vector(1 DOWNTO 0);
+	        ALUFN : IN std_logic_vector (1 DOWNTO 0);
 	        cout : OUT std_logic;
-                S : OUT std_logic_vector (n-1 DOWNTO 0)
+                res : OUT std_logic_vector (n-1 DOWNTO 0) 
 			);
 END shifter;
 --------------------------------------
 ARCHITECTURE shifterArch OF shifter IS
 
-        -- signal k : integer := 0;
-        -- for l in 0 to n-1 generate
-        --         if x(i) = '1' then
-        --                 k <= k + l ** 2;
-        -- end generate;
+        signal zeros : std_logic_vector (n-1 DOWNTO 0 ) := (others => '0');
+        constant log_n: integer := integer(ceil(log2(real(n))))
 
 BEGIN
-	-- if dir = '0' then             -- shift left
-        --         for j in 0 to k-1 generate
-        --                 cout <= y(n-1);
-        --                 for i in n-1 downto 1 generate
-        --                         res(i) <= y(i-1);
-        --                 end generate;
-        --                 res(0) <= '0';
-        --         end generate;
-        -- else                        -- dir = 1 -> shift right
-        --         for j in 0 to k-1 generate
-
-        --                 cout <= y(0);
-        --                 for i in 0 to n-2 generate
-        --                         res(i) <= y(i+1);
-        --                 end generate;
-        --                 res(n-1) <= '0';
-
-        --         end generate;
+         
+     loop0: for i in 0 to log_n generate
+                        cout <= y( n - 2**i )  when (x(i)= '1' and ALUFN ="00") else
+                                     y( 2**i - 1) when (x(i)= '1' and ALUFN = "01") else
+                                     UNAFFECTED; 
+                                      
+                        res <= y( n - 2*i - 1 downto 0) & zeros( 0 to 2*i ) when (x(i)= '1' and ALUFN ="00") else
+                                    zeros( 0 to 2*i) & y( n - 1 downto 2*i)  when (x(i)= '1' and ALUFN = "01") else
+                                    UNAFFECTED;   
+                
+                end generate;
+        
 END shifterArch;
---
+++ TB/tb_shifter.vhd
@@ -0,0 +1,66 @@
+-- shifter unit Test Bench
+LIBRARY IEEE;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_arith.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY tb_shifter IS
+    constant n : integer := 3;
+END tb_shifter;
+
+ARCHITECTURE tb_shifter_arch OF tb_shifter IS
+
+COMPONENT shifter
+    GENERIC (n : INTEGER := 3);
+    PORT (x, y : IN std_logic_vector (n-1 DOWNTO 0);
+	        ALUFN : IN std_logic_vector (1 DOWNTO 0);
+	        cout : OUT std_logic;
+            res : OUT std_logic_vector (n-1 DOWNTO 0) 
+			);
+END COMPONENT;
+
+SIGNAL x, y : std_logic_vector (n-1 DOWNTO 0);
+SIGNAL ALUFN : std_logic_vector (1 DOWNTO 0); 
+SIGNAL res  : std_logic_vector (n-1 DOWNTO 0);
+SIGNAL cout : std_logic;
+
+begin
+    tester : logic generic map (n) port map(x, y, ALUFN, cout, res);
+                     
+    --------- start of stimulus section  ------------------               
+    testbench : process
+    begin
+        x <= (others => '0');
+		y <= (others => '0');
+        
+        
+        ALUFN <= '00';                   -- left shift testing
+        for i in 0 to 10 loop            -- Iterating over x values from 0 to 10
+            for i in 0 to 2**5 loop      -- Iterating over y values for each number of shifts
+                wait for 100 ns;
+                    y <= y + 1;
+            end loop;
+            x <= x + 1;
+        end loop;
+        
+        x <= (others => '0');
+		y <= (others => '0');
+
+        ALUFN <= '01';                   -- right shift testing
+        for i in 0 to 10 loop            -- Iterating over x values from 0 to 10
+            for i in 0 to 2**5 loop      -- Iterating over y values for each number of shifts
+                wait for 100 ns;
+                    y <= y + 1;
+            end loop;
+            x <= x + 1;
+        end loop;
+    end process testbench;            
+        ---------- end of stimulus section  ---------------------------
+          
+
+end tb_shifter_arch;
+    
+    
+      
+     
+	
